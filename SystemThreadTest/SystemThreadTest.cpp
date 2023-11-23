c7848b21f83ad08799f1e6b5cec4315b2f9af40c
zienkiewicz <k09028449@gmail.com>
2023-11-23 17:16:15 +0100

    Boilerplate driver stuff

--- SystemThreadTest/SystemThreadTest.cpp
+++ SystemThreadTest/SystemThreadTest.cpp
@@ -1,6 +1,23 @@
 #include <ntddk.h>
 
+void DriverUnload(PDRIVER_OBJECT DriverObject);
+NTSTATUS CreateClose(PDEVICE_OBJECT, PIRP);
+
 extern "C" NTSTATUS
 DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	DriverObject->DriverUnload = DriverUnload;
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = CreateClose;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = CreateClose;
+
 	return STATUS_SUCCESS;
+}
+
+void DriverUnload(PDRIVER_OBJECT DriverObject) {
+	return;
+}
+
+NTSTATUS CreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	return NTSTATUS();
 }
