2304082a3ef3186329b4beeb89bcc44a2ab7fd77
zienkiewicz <zienkiewitch@outlook.com>
2023-11-24 14:15:55 +0100

    IOCTL handling

--- SystemThreadTest/SystemThreadTest.cpp
+++ SystemThreadTest/SystemThreadTest.cpp
@@ -1,5 +1,8 @@
 #include <ntddk.h>
 
+#define MY_DEVICE_TYPE 0x8123
+#define MY_IOCTL_FUNCTION CTL_CODE(MY_DEVICE_TYPE, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
 UNICODE_STRING Name = RTL_CONSTANT_STRING(L"\\Device\\SystemThreadTest");
 UNICODE_STRING SymbolicLink = RTL_CONSTANT_STRING(L"\\??\\SystemThreadTest");
 
@@ -14,7 +17,8 @@ DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
 	DriverObject->DriverUnload = DriverUnload;
 	DriverObject->MajorFunction[IRP_MJ_CREATE] = CreateClose;
 	DriverObject->MajorFunction[IRP_MJ_CLOSE] = CreateClose;
-
+	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = WorkIoctl;
+	
 	PDEVICE_OBJECT DeviceObject;
 	NTSTATUS status = IoCreateDevice(
 		DriverObject,
@@ -53,4 +57,46 @@ NTSTATUS CreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
 	Irp->IoStatus.Information = 0;
 	IoCompleteRequest(Irp, IO_NO_INCREMENT);
 	return NTSTATUS();
+}
+
+VOID ThreadFunction(PVOID StartContext) {
+	UNREFERENCED_PARAMETER(StartContext);
+	HANDLE id = PsGetCurrentProcessId();
+	KdPrint(("SystemThreadTest: PID %lu", id));
+	PsTerminateSystemThread(STATUS_SUCCESS);
+}
+
+NTSTATUS WorkIoctl(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+
+	PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
+
+	if (stack->Parameters.DeviceIoControl.IoControlCode == MY_IOCTL_FUNCTION) {
+		HANDLE a, b;
+		(void)PsCreateSystemThread(
+			&a,
+			THREAD_ALL_ACCESS,
+			NULL,
+			NtCurrentProcess(),
+			NULL,
+			ThreadFunction,
+			NULL
+		);
+		ZwClose(a);
+
+		(void)PsCreateSystemThread(
+			&b,
+			THREAD_ALL_ACCESS,
+			NULL,
+			NULL,
+			NULL,
+			ThreadFunction,
+			NULL
+		);
+		ZwClose(b);
+	}
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
 }
